cc6e1cce20e19c152529dcc8ed3a1bfaa496b3fb
laurensiavee <92752405+laurensiavee@users.noreply.github.com>
2023-06-27 14:18:15 +0700

    upload image

---
+++ Models/Product.js
@@ -0,0 +1,15 @@
+const { Decimal128 } = require('mongodb');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const productSchema = new mongoose.Schema({
+    productName: String,
+    price: Decimal128,
+    description: String,
+    qty: Decimal128,
+    productImage: String,
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const cors = require("cors");
 const path = require("path");
 const connectDB = require("./db");
 const bodyParser = require("body-parser");
+const multer = require("multer");
+require('dotenv').config();
 
 const app = express();
 const User = require("./Models/User");
+const Product = require("./Models/Product");
 // Connect to the database
 connectDB();
 
@@ -13,11 +16,8 @@ connectDB();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-// app.use(express.static(path.join(__dirname, "../E-Commerce-Frontend/build")));
-
-// For rendering React file
-
 app.use(express.static(path.join(__dirname, "../E-Commerce-Frontend/build")));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.get("*", (req, res) => {
   res.sendFile(
@@ -25,11 +25,39 @@ app.get("*", (req, res) => {
   );
 });
 
+// Image store
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './uploads');
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      new Date().toISOString().replace(/:/g, '-') + '-' + file.originalname
+    );
+  },
+});
+
+const filefilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({ storage: storage, filefilter: filefilter });
+
+// Register route
 app.get("/register", (req, res) => {
   console.log("register");
 });
 
-// Register route
 app.post("/register", async (req, res) => {
   try {
     console.log("Received data:", req.body);
@@ -54,6 +82,36 @@ app.post("/register", async (req, res) => {
   } catch (error) {
     console.error("Error:", error);
     res.status(500).json({ message: "Registration failed" });
+  }
+});
+
+// Add Product
+app.post("/add-product", upload.single('ProductImage'), async (req, res) => {
+  try {
+    console.log("Received data:", req.body);
+    console.log("Received filename:", req.file.filename);
+    
+    const body = req.body;
+    const imgPath = String(req.protocol + '://' + req.get('host') + '/uploads/' + req.file.filename)
+
+    console.log("save to::", imgPath);
+
+    // Create a new user instance
+    const product = new Product({
+      productName: body.ProductName,
+      price: body.Price,
+      description: body.Description,
+      qty: body.Qty,
+      productImage: req.protocol + '://' + req.get('host') + '/uploads/' + req.file.filename
+    });
+
+    // Save the user to the database
+    await product.save();
+
+    res.json({ message: "Add Product successful" });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ message: "Add Product failed" });
   }
 });
 
