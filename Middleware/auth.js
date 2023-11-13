9e0e0a18a390258a12180958da8b05bfe2a22ec8
Vincent7564 <SoonHee438@gmail.com>
2023-11-13 21:36:26 +0700

    feat:add condition expired

--- Middleware/auth.js
+++ Middleware/auth.js
@@ -1,9 +1,8 @@
-// authMiddleware.js
-
 const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
-  const token = req.headers.authorization;
+  const token = req.headers.authorization.split(' ')[1];
+  console.log("Token "+token)
   if (!token) {
     return res.status(401).json({ message: 'Unauthorized' });
   }
@@ -12,6 +11,9 @@ const verifyToken = (req, res, next) => {
     console.log(token)
     if (err) {
       console.log(err)
+      if (err.name === 'TokenExpiredError') {
+        return res.status(401).json({ message: 'Token has expired' });
+      }
       return res.status(401).json({ message: 'Token is not valid' });
     }
 
